e0c8d80cf7a25d3d29d61650158ae7cda0f9a83c
Ron Buckton <ron.buckton@microsoft.com>
2017-05-25 11:33:36 -0400

    Upgrade to TypeScript 2.3

--- src/tests/typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main\ambient\chai\chai.d.ts" />
-/// <reference path="main\ambient\mocha\mocha.d.ts" />
-/// <reference path="main\ambient\node\node.d.ts" />
-/// <reference path="main\ambient\source-map-support\source-map-support.d.ts" />
